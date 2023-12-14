c5864d7fa068e76ab12b1945b2d19be40e1f5c24
rjpcasalino <ryan@boringtranquility.io>
2023-12-13 23:31:50 -0800

    make a dev shell and update defaults

--- flake.nix
+++ flake.nix
@@ -5,35 +5,38 @@
     flake-utils.url = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
-    with flake-utils.lib; eachSystem allSystems (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      tex = pkgs.texlive.combine {
-          inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
-      };
-    in rec {
-      packages = {
-        document = pkgs.stdenvNoCC.mkDerivation rec {
-          name = "latex-document";
-          src = self;
-          buildInputs = [ pkgs.coreutils pkgs.latex2html tex ];
-          phases = ["unpackPhase" "buildPhase" "installPhase"];
-          buildPhase = ''
-            export PATH="${pkgs.lib.makeBinPath buildInputs}";
-            mkdir -p .cache/texmf-var
-            env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-              OSFONTDIR=${pkgs.commit-mono}/share/fonts \
-              latexmk -interaction=nonstopmode -pdf -lualatex \
-              document.tex; \
-              latex2html -noinfo document.tex
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp document.pdf $out/
-            cp -r document $out/
-          '';
-        };
-      };
-      defaultPackage = packages.document;
-    });
+    with flake-utils.lib; eachSystem allSystems
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          tex = pkgs.texlive.combine {
+            inherit (pkgs.texlive) scheme-minimal latex-bin latexmk;
+          };
+        in
+        rec {
+          packages = {
+            document = pkgs.stdenvNoCC.mkDerivation rec {
+              name = "latex-document";
+              src = self;
+              buildInputs = [ pkgs.coreutils pkgs.latex2html tex ];
+              phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+              buildPhase = ''
+                export PATH="${pkgs.lib.makeBinPath buildInputs}";
+                mkdir -p .cache/texmf-var
+                env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
+                  OSFONTDIR=${pkgs.commit-mono}/share/fonts \
+                  latexmk -interaction=nonstopmode -pdf -lualatex \
+                  document.tex; \
+                  latex2html -noinfo document.tex
+              '';
+              installPhase = ''
+                mkdir -p $out
+                cp document.pdf $out/
+                cp -r document $out/
+              '';
+            };
+          };
+          packages.default = packages.document;
+          devShell = with pkgs; mkShell { packages = [packages.document.buildInputs]; shellHook = ''echo -n Hello LaTeX'';};
+        });
 }
