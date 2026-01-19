4f9b386764bd6cf4f1b30b6490c8a402b57fea9e
rjpc <ryan@rjpc.net>
2026-01-18 22:45:11 -0800

    get HTML stuff working; followed this https://ctan.math.illinois.edu/macros/latex/contrib/lwarp/lwarp.pdf

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
           pkgs = nixpkgs.legacyPackages.${system};
           # re: latexmk see: https://latex.us/support/latexmk/INSTALL
           tex = pkgs.texlive.combine {
-            inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+            inherit (pkgs.texlive) scheme-full latex-bin latexmk lwarp;
           };
         in
         rec {
@@ -19,7 +19,7 @@
             document = pkgs.stdenvNoCC.mkDerivation rec {
               name = "latex-document";
               src = self;
-              buildInputs = [ pkgs.coreutils tex ];
+              buildInputs = [ pkgs.coreutils tex pkgs.poppler_utils ];
               phases = [ "unpackPhase" "buildPhase" "installPhase" ];
               buildPhase = ''
                 export PATH="${pkgs.lib.makeBinPath buildInputs}";
