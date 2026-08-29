6781a9ca5bcfc552e6773bbda9c185308edbe477
rjpc <ryan@rjpc.net>
2026-08-28 19:19:56 -0700

    flake updates and pdf upload

--- flake.nix
+++ flake.nix
@@ -1,35 +1,57 @@
 {
   description = "Get Started with LaTeX";
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     with flake-utils.lib; eachSystem allSystems
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          # re: latexmk see: https://latex.us/support/latexmk/INSTALL
-          tex = pkgs.texlive.combine {
-            inherit (pkgs.texlive) scheme-full latex-bin latexmk lwarp;
-          };
+          tex = pkgs.texliveSmall.withPackages (
+            ps: with ps; [
+              scheme-full latex-bin latexmk lwarp
+            ]
+          );
+          # Aggregate modern typefaces into a single directory
+          fonts = pkgs.runCommand "resume-fonts" {} ''
+            mkdir -p $out/share/fonts
+            for d in \
+              ${pkgs.inter}/share/fonts \
+              ${pkgs.montserrat}/share/fonts \
+              ${pkgs.commit-mono}/share/fonts \
+              ${pkgs.geist-font}/share/fonts \
+              ${pkgs.plus-jakarta-sans}/share/fonts \
+              ${pkgs.ibm-plex}/share/fonts \
+              ${pkgs.jetbrains-mono}/share/fonts \
+              ${pkgs.fira-code}/share/fonts \
+              ${pkgs.roboto}/share/fonts \
+              ${pkgs.lato}/share/fonts; do
+              if [ -d "$d" ]; then
+                find "$d" -type f \( -name '*.ttf' -o -name '*.otf' -o -name '*.ttc' \) \
+                  -exec cp -f {} $out/share/fonts \;
+              fi
+            done
+          '';
         in
         rec {
           packages = {
             document = pkgs.stdenvNoCC.mkDerivation rec {
-              name = "latex-document";
+              name = "resume-document";
               src = self;
-              buildInputs = [ pkgs.coreutils pkgs.poppler-utils tex ];
+              buildInputs = [ pkgs.coreutils pkgs.poppler-utils tex fonts ];
               phases = [ "unpackPhase" "buildPhase" "installPhase" ];
               buildPhase = ''
                 export PATH="${pkgs.lib.makeBinPath buildInputs}";
-                mkdir -p .cache/texmf-var
-                env TEXMFHOME=.cache TEXMFVAR=.cache/texmf-var \
-                  SOURCE_DATE_EPOCH=$(date +%s) \
-                  OSFONTDIR=${pkgs.commit-mono}/share/fonts \
-                  latexmk -interaction=nonstopmode -pdf -lualatex \
-                  resume.tex;
-              '';
+                export OSFONTDIR="${fonts}/share/fonts";
+                export TEXMFHOME="$(pwd)/.cache"
+                export TEXMFVAR="$(pwd)/.cache/texmf-var"
+                mkdir -p "$TEXMFVAR"
+                # Force font cache update during Nix build
+                luaotfload-tool --update --force
+                latexmk -interaction=nonstopmode -pdf -lualatex resume.tex
+                '';
               installPhase = ''
                 mkdir -p $out
                 cp resume.pdf $out/
@@ -37,6 +59,22 @@
             };
           };
           packages.default = packages.document;
-          devShell = with pkgs; mkShell { packages = [ packages.document.buildInputs ]; shellHook = ''SOURCE_DATE_EPOCH=$(date +%s); printf "\t%s\n\t%s\n", "Hello LaTeX", "run latexmk -interaction=nonstopmode -pdf -pvc -lualatex <your_tex_doc.tex>"''; };
+          devShell = with pkgs; mkShell {
+            # https://discourse.nixos.org/t/help-with-nix-shell-uses-a-nested-list-in-attribute-buildinputs/77810/8
+            inputsFrom = [ packages.document ];
+            shellHook = ''
+              # Expose the Nix fonts to LuaLaTeX in the local shell
+              export OSFONTDIR="${fonts}/share/fonts"
+              # Isolate the TeX cache to the local project directory
+              export TEXMFHOME="$(pwd)/.cache"
+              export TEXMFVAR="$(pwd)/.cache/texmf-var"
+              mkdir -p "$TEXMFVAR"
+              # Force the font database to refresh when entering the shell
+              echo "Updating LuaLaTeX font cache..."
+              luaotfload-tool --update
+              export SOURCE_DATE_EPOCH=$(date +%s);
+              printf "\n\t%s\n\t%s\n\n" "Hello LaTeX" "run: latexmk -interaction=nonstopmode -pdf -pvc -lualatex resume.tex"
+            '';
+          };
         });
 }
